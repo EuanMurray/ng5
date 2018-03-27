9dae730ee8f66f049c2ab6b5a74bdaed1ecbffa8
Euan Murray <euan.murray@itftennis.com>
2018-03-27 10:09:48 +0100

    Added routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,10 +17,17 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
             style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-          ]))]), {optional: true})
+          ]))]), {optional: true}),
+
+          query(':leave', stagger('300ms', [
+            animate('.6s ease-in', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+              style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+            ]))]), {optional: true})
       ])
     ])
-    
+
   ]
 })
 export class HomeComponent implements OnInit {
@@ -28,7 +35,7 @@ export class HomeComponent implements OnInit {
   itemCount: number;
   btnText: string = 'Add an item';
   goalText: string = 'My first life goal';
-  goals = [];
+  goals = ['Skydive','Buy a boat'];
 
   constructor() { }
 
@@ -40,6 +47,10 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i,1);
   }
 
 }
